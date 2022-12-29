5fd8abea96d884db2c6ae87a10b0ecc41315eeed
saleem786786 <saleemakhtar8686@gmail.com>
2022-12-30 02:50:49 +0500

    Drawing game board

--- game.php
+++ game.php
@@ -10,3 +10,24 @@ $board = [
 // Define the player symbols
 $player1 = 'X';
 $player2 = 'O';
+
+// Initialize the current player as player 1
+$currentPlayer = $player1;
+
+// Initialize a variable to track the game status
+$gameOver = false;
+
+// Display the final board
+displayBoard($board);
+
+// Function to display the board
+function displayBoard($board) {
+  // Loop through the rows
+  for ($i = 0; $i < 3; $i++) {
+    // Loop through the columns
+    for ($j = 0; $j < 3; $j++) {
+      echo $board[$i][$j] . ' ';
+    }
+    echo "\n";
+  }
+}
