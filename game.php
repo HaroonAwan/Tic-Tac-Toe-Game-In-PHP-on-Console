274a6cbf2f92596cd4b2cbbb3a8ef0ba8ef2fbb5
saleem786786 <saleemakhtar8686@gmail.com>
2022-12-30 02:33:44 +0500

    Create game.php

---
+++ game.php
@@ -0,0 +1,12 @@
+<?php
+
+// Define the board as a 2D array with 3 rows and 3 columns
+$board = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
+];
+
+// Define the player symbols
+$player1 = 'X';
+$player2 = 'O';
